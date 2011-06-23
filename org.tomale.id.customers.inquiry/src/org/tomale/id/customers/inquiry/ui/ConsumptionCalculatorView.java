611483b7211a9932aa50f07f8565bcfb78279a50
Ferdinand Tomale <beowulf1416@gmail.com>
2011-06-23 16:57:02 -0400

    added customer inquiry project

---
+++ org.tomale.id.customers.inquiry/src/org/tomale/id/customers/inquiry/ui/ConsumptionCalculatorView.java
@@ -0,0 +1,20 @@
+package org.tomale.id.customers.inquiry.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ConsumptionCalculatorView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
