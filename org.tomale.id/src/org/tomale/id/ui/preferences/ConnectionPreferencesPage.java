426839037e6d73d8fe8d185bb30c8536e0f593f5
Ferdinand Tomale <beowulf1416@gmail.com>
2011-06-23 17:10:28 -0400

    added preferences pages

---
+++ org.tomale.id/src/org/tomale/id/ui/preferences/ConnectionPreferencesPage.java
@@ -0,0 +1,24 @@
+package org.tomale.id.ui.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class ConnectionPreferencesPage extends PreferencePage
+	implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	protected Control createContents(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
