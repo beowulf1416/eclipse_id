1c8433c20a8c0c1ee08d56cbef674a3e8f7e4e21
Ferdinand L. Tomale <beowulf1416@gmail.com>
2011-06-24 17:12:53 +0800

    modified view parts

--- org.tomale.id.customers.inquiry/src/org/tomale/id/customers/inquiry/ui/CustomerBrowserView.java
+++ org.tomale.id.customers.inquiry/src/org/tomale/id/customers/inquiry/ui/CustomerBrowserView.java
@@ -2,11 +2,56 @@ package org.tomale.id.customers.inquiry.ui;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import swing2swt.layout.BoxLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.CoolBar;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 
 public class CustomerBrowserView extends ViewPart {
+	private Table table;
+	private Text text;
+	public CustomerBrowserView() {
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new FillLayout());
+		
+		Composite container = new Composite(parent, SWT.NONE);
+		GridLayout gl_container = new GridLayout(1,false);
+		container.setLayout(gl_container);
+		
+		Composite composite = new Composite(container, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		composite.setLayout(new GridLayout(3, false));
+		
+		Combo combo = new Combo(composite, SWT.READ_ONLY);
+		GridData gd_combo = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_combo.widthHint = 25;
+		combo.setLayoutData(gd_combo);
+		
+		text = new Text(composite, SWT.BORDER);
+		GridData gd_text = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_text.widthHint = 75;
+		text.setLayoutData(gd_text);
+		
+		Button btnFind = new Button(composite, SWT.NONE);
+		btnFind.setText("Find");
+		
+		table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
 		// TODO Auto-generated method stub
 
 	}
@@ -16,5 +61,4 @@ public class CustomerBrowserView extends ViewPart {
 		// TODO Auto-generated method stub
 
 	}
-
 }
--- org.tomale.id.customers.inquiry/src/org/tomale/id/customers/inquiry/ui/CustomerView.java
+++ org.tomale.id.customers.inquiry/src/org/tomale/id/customers/inquiry/ui/CustomerView.java
@@ -2,11 +2,33 @@ package org.tomale.id.customers.inquiry.ui;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.custom.CTabItem;
 
 public class CustomerView extends ViewPart {
+	public CustomerView() {
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		CTabFolder tabFolder = new CTabFolder(parent, SWT.BORDER);
+		tabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
+		
+		CTabItem tbtmCommon = new CTabItem(tabFolder, SWT.NONE);
+		tbtmCommon.setText("Common");
+		
+		CTabItem tbtmBilling = new CTabItem(tabFolder, SWT.NONE);
+		tbtmBilling.setText("Billings");
+		
+		CTabItem tbtmPayments = new CTabItem(tabFolder, SWT.NONE);
+		tbtmPayments.setText("Payments");
 		// TODO Auto-generated method stub
 
 	}
