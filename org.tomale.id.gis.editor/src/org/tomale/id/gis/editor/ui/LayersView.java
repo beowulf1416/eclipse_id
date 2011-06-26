bf0879751127a3e11e92bc1ae8a721b1b558ab93
Ferdinand Tomale <beowulf1416@gmail.com>
2011-06-26 16:48:56 -0400

    modified main menu. added show map layers view command and handler.

--- org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/ui/LayersView.java
+++ org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/ui/LayersView.java
@@ -1,14 +1,21 @@
 package org.tomale.id.gis.editor.ui;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 public class LayersView extends ViewPart {
 
+	public final static String VIEW_ID = "org.tomale.id.gis.editor.view.layers";
+	
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		
+		parent.setLayout(new FillLayout());
+		
+		Composite container = new Composite(parent, SWT.NONE);
+		
 	}
 
 	@Override
