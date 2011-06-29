061b808aca7a39ba8b89492a2863994464d47ac8
Ferdinand L. Tomale <beowulf1416@gmail.com>
2011-06-29 14:44:06 +0800

    added layer dialog and layer preference page

---
+++ org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/dialogs/LayerDialog.java
@@ -0,0 +1,75 @@
+package org.tomale.id.gis.editor.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+
+public class LayerDialog extends Dialog {
+	private Text txtName;
+
+	/**
+	 * Create the dialog.
+	 * @param parentShell
+	 */
+	public LayerDialog(Shell parentShell) {
+		super(parentShell);
+		setShellStyle(SWT.TITLE | SWT.APPLICATION_MODAL);
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		GridLayout gridLayout = (GridLayout) container.getLayout();
+		gridLayout.numColumns = 2;
+		
+		Label lblName = new Label(container, SWT.NONE);
+		lblName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblName.setText("Name");
+		
+		txtName = new Text(container, SWT.BORDER);
+		txtName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblProvider = new Label(container, SWT.NONE);
+		lblProvider.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblProvider.setText("Provider");
+		
+		Combo cboProvider = new Combo(container, SWT.NONE);
+		cboProvider.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		return container;
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+				true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+
+}
---
+++ org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/preferences/LayerPreferencePage.java
@@ -0,0 +1,94 @@
+package org.tomale.id.gis.editor.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class LayerPreferencePage extends PreferencePage {
+
+	TableViewer _tblvwr;
+	
+	@Override
+	protected Control createContents(Composite parent) {
+		parent.setLayout(new FillLayout());
+		
+		Composite container = new Composite(parent, SWT.NONE);
+		
+		_tblvwr = new TableViewer(container);
+		_tblvwr.setContentProvider(new LayerContentProvider());
+		_tblvwr.setLabelProvider(new LayerLabelProvider());
+		
+		return null;
+	}
+	
+	public class LayerContentProvider implements IStructuredContentProvider {
+
+		@Override
+		public void dispose() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	public class LayerLabelProvider implements ITableLabelProvider {
+
+		@Override
+		public void addListener(ILabelProviderListener listener) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void dispose() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean isLabelProperty(Object element, String property) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void removeListener(ILabelProviderListener listener) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getColumnText(Object element, int columnIndex) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+}
