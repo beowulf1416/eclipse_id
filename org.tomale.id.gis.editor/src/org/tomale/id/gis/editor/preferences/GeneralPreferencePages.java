26ce938dfbb4f2729b89c2a823866f137378d517
Ferdinand L. Tomale <beowulf1416@gmail.com>
2011-06-30 13:27:11 +0800

    modified preference pages

--- org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/preferences/GeneralPreferencePages.java
+++ org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/preferences/GeneralPreferencePages.java
@@ -1,15 +1,72 @@
 package org.tomale.id.gis.editor.preferences;
 
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.ColorDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 
 public class GeneralPreferencePages extends PreferencePage {
+	
+	Canvas canvas;
+	
+	public GeneralPreferencePages() {
+	}
 
 	@Override
 	protected Control createContents(Composite parent) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Composite container = new Composite(parent, SWT.NONE);
+		GridLayout gl_container = new GridLayout();
+		container.setLayout(gl_container);
+		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		
+		Group grpColors = new Group(container, SWT.NONE);
+		grpColors.setText("Colors");
+		grpColors.setLayout(new GridLayout(3, false));
+		
+		Label lblBackground = new Label(grpColors, SWT.NONE);
+		lblBackground.setText("Background");
+		
+		canvas = new Canvas(grpColors, SWT.BORDER);
+		GridData gd_canvas = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_canvas.heightHint = 24;
+		gd_canvas.widthHint = 24;
+		canvas.setLayoutData(gd_canvas);
+		
+		Button button = new Button(grpColors, SWT.NONE);
+		button.setText("...");
+		button.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				chooseColor();
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				chooseColor();				
+			}
+		});
+		
+		return container;
 	}
 
+	private void chooseColor(){
+		
+		ColorDialog dlg = new ColorDialog(getShell());
+		RGB rgb = dlg.open();
+		canvas.setBackground(new Color(getShell().getDisplay(), rgb));
+		
+	}
 }
--- org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/preferences/LayerPreferencePage.java
+++ org.tomale.id.gis.editor/src/org/tomale/id/gis/editor/preferences/LayerPreferencePage.java
@@ -8,25 +8,73 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 
 public class LayerPreferencePage extends PreferencePage {
-
+	
 	TableViewer _tblvwr;
 	
+	public LayerPreferencePage() {
+	}
+
 	@Override
 	protected Control createContents(Composite parent) {
-		parent.setLayout(new FillLayout());
 		
 		Composite container = new Composite(parent, SWT.NONE);
+		parent.setLayout(new GridLayout());
+		container.setLayout(new GridLayout(3, false));
 		
 		_tblvwr = new TableViewer(container);
+		_tblvwr.setColumnProperties(new String[] {});
 		_tblvwr.setContentProvider(new LayerContentProvider());
 		_tblvwr.setLabelProvider(new LayerLabelProvider());
+		Table tbl = _tblvwr.getTable();
+		tbl.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true, 3, 1));
+		tbl.setHeaderVisible(true);
 		
-		return null;
+		TableColumn tblclmnEdit = new TableColumn(tbl, SWT.NONE);
+		tblclmnEdit.setToolTipText("Edit");
+		tblclmnEdit.setResizable(false);
+		tblclmnEdit.setWidth(32);
+		
+		TableColumn tblclmnRemove = new TableColumn(tbl, SWT.NONE);
+		tblclmnRemove.setResizable(false);
+		tblclmnRemove.setToolTipText("Remove");
+		tblclmnRemove.setWidth(32);
+		
+		TableColumn tblclmnName = new TableColumn(tbl, SWT.NONE);
+		tblclmnName.setWidth(100);
+		tblclmnName.setText("Name");
+		
+		TableColumn tblclmnVisible = new TableColumn(tbl, SWT.NONE);
+		tblclmnVisible.setWidth(100);
+		tblclmnVisible.setText("Visible");
+		
+		TableColumn tblclmnReadOnly = new TableColumn(tbl, SWT.NONE);
+		tblclmnReadOnly.setWidth(100);
+		tblclmnReadOnly.setText("Read Only");
+		
+		Button btnAdd = new Button(container, SWT.NONE);
+		btnAdd.setText("Add");
+		
+		Button btnRemove = new Button(container, SWT.NONE);
+		btnRemove.setText("Remove");
+		
+		Button btnEdit = new Button(container, SWT.NONE);
+		btnEdit.setText("Edit");
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+		
+		return container;
 	}
 	
 	public class LayerContentProvider implements IStructuredContentProvider {
