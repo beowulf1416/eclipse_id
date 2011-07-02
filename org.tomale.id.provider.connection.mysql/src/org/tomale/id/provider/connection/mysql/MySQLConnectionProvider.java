08e95cb848a9e91ba52255009156a33c1daec9b5
ferd <beowulf1416@gmail.com>
2011-07-03 05:27:20 +0800

    added mysql connection provider

---
+++ org.tomale.id.provider.connection.mysql/src/org/tomale/id/provider/connection/mysql/MySQLConnectionProvider.java
@@ -0,0 +1,23 @@
+package org.tomale.id.provider.connection.mysql;
+
+import org.tomale.id.dal.IConnectionProvider;
+
+public class MySQLConnectionProvider implements IConnectionProvider {
+
+	public MySQLConnectionProvider() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
