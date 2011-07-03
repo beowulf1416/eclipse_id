7164cb94f15fcdd280c1b28cc44c40b7f5346792
ferd <beowulf1416@gmail.com>
2011-07-03 11:07:38 +0800

    added file connection provider

---
+++ org.tomale.id.provider.connection.file/src/org/tomale/id/provider/connection/file/FileConnectionProvider.java
@@ -0,0 +1,19 @@
+package org.tomale.id.provider.connection.file;
+
+import org.tomale.id.dal.IConnectionProvider;
+
+public class FileConnectionProvider implements IConnectionProvider {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
